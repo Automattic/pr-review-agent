b89be3231a444b4d6ff0515ca851989ceda1e87c
apeatling <apeatling@gmail.com>
2025-02-04 13:22:49 -0800

    Clearer info and fix for git server restart

--- start-git.sh
+++ start-git.sh
@@ -3,4 +3,5 @@ source $( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )/.env
 
 # Run the Git MCP server with the project repository path
 cd $( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )/mcp-servers/src/git
+rm -rf .venv
 uv run mcp-server-git --repository $PROJECT_REPOSITORY_PATH
