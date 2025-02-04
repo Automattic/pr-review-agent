2b29b99a4628049f2fb0a67ed2b5765c87012661
apeatling <apeatling@gmail.com>
2025-02-04 13:41:35 -0800

    Add missing export for access token

--- start-github.sh
+++ start-github.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 source $( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )/.env
 
-# Set the GitHub Personal Access Token in a .env file in the root of the pr-review-agent folder
-GITHUB_PERSONAL_ACCESS_TOKEN=$GITHUB_PERSONAL_ACCESS_TOKEN
+export GITHUB_PERSONAL_ACCESS_TOKEN=$GITHUB_PERSONAL_ACCESS_TOKEN
 
 # Run the GitHub MCP server
 cd $( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )/mcp-servers/src/github
